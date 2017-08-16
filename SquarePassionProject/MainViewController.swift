6087fa00ae8c5427d9aec197a565e165370d44f7
Kelly Lampotang <klampotang@gmail.com>
2017-08-16 19:16:36 -0400

    Main view controller

---
+++ SquarePassionProject/MainViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MainViewController.swift
+//  SquarePassionProject
+//
+//  Created by Kelly Lampotang on 8/16/17.
+//  Copyright © 2017 Kelly Lampotang. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController : UIViewController {
+    
+}
