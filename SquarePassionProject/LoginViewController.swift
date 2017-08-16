49e26dacca35b986856f8c0017ac66ac3df7fcab
Kelly Lampotang <klampotang@gmail.com>
2017-08-16 18:20:31 -0400

    initial commit

---
+++ SquarePassionProject/LoginViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewController.swift
+//  SquarePassionProject
+//
+//  Created by Kelly Lampotang on 8/16/17.
+//  Copyright © 2017 Kelly Lampotang. All rights reserved.
+//
+
+import Foundation
