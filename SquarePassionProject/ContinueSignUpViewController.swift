da55539e6c47aee00d25ebba0c2944b75923933f
Kelly Lampotang <klampotang@gmail.com>
2017-08-17 09:31:10 -0400

    Save signup information and segue setup

--- SquarePassionProject/ContinueSignUpViewController.swift
+++ SquarePassionProject/ContinueSignUpViewController.swift
@@ -10,15 +10,43 @@ import UIKit
 
 class ContinueSignUpViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var genderSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    var username : String?
+    var password : String?
+    var age : String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        errorLabel.isHidden = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func continueButtonPressed(_ sender: Any) {
+        if(usernameTextField.text == "" ||
+        passwordTextField.text == "" ||
+        confirmPasswordTextField.text == "" ||
+            ageTextField.text == "") {
+            errorLabel.text = "Please fill out all fields"
+            errorLabel.isHidden = false
+        }
+        if(passwordTextField.text == confirmPasswordTextField.text) {
+            errorLabel.isHidden = true
+            username = usernameTextField.text
+            password = passwordTextField.text
+            age = ageTextField.text
+            performSegue(withIdentifier: "goToMain", sender: self)
+        } else {
+            errorLabel.isHidden = false
+        }
     }
     
 
