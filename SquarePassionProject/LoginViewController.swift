6f0a06bc927af9ef663f0da130610d8cd7faaf42
Kelly Lampotang <klampotang@gmail.com>
2017-08-18 02:29:49 -0400

    animation

--- SquarePassionProject/LoginViewController.swift
+++ SquarePassionProject/LoginViewController.swift
@@ -12,16 +12,20 @@ class LoginViewController : UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var welcomeLabel: UILabel!
     let defaults = UserDefaults.standard
-    
+    let welcomeStrings : [String] = ["Welcome!", "Let's find you a bed", "You've found your haven", "Let us help you", "Hope your day is going well!", "You look great today!"]
+    var count = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         messageLabel.isHidden = true
+        welcomeLabel.adjustsFontSizeToFitWidth = true
 
         //Looks for single or multiple taps.
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
         view.addGestureRecognizer(tap)
         // Do any additional setup after loading the view.
+        var welcomeTimer = Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(LoginViewController.welcomeFade), userInfo: nil, repeats: true)
     }
     func dismissKeyboard() {
         //Causes the view (or one of its embedded text fields) to resign the first responder status.
@@ -31,11 +35,25 @@ class LoginViewController : UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    func welcomeFade() {
+        UIView.animate(withDuration: 1.0, delay: 0.0, options: UIViewAnimationOptions.curveEaseOut, animations: {
+            self.welcomeLabel.alpha = 1.0
+        }, completion: {
+            (finished: Bool) -> Void in
+            
+            self.welcomeLabel.text = self.welcomeStrings[self.count%self.welcomeStrings.count]
+            self.count+=1
+            // Fade in
+            UIView.animate(withDuration: 1.0, delay: 0.0, options: UIViewAnimationOptions.curveEaseIn, animations: {
+                self.welcomeLabel.alpha = 0.0
+            }, completion: nil)
+        })
+
+    }
     
     
     @IBAction func loginButtonClicked(_ sender: Any) {
         //Check username
-        //TODO
         let usernametextfieldlowercase = usernameTextField.text?.lowercased()
         let usernameKey = usernametextfieldlowercase! + "Username"
         let passwordReal = defaults.string(forKey: usernameKey)
