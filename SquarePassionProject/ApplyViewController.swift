d0d2b37d362ff3bc8a88cb1f225d542f512a5c77
Kelly Lampotang <klampotang@gmail.com>
2017-08-17 22:55:10 -0400

    tbd shelters

--- SquarePassionProject/ApplyViewController.swift
+++ SquarePassionProject/ApplyViewController.swift
@@ -12,6 +12,7 @@ import CoreLocation
 class ApplyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, CLLocationManagerDelegate {
     var shelterNames: [String] = ["Odyssey Villas Intact Families", "The Salvation Army Metro Atlanta Red Shield Service", "Serenity House of Atlanta", "The Shepherd's Inn Downtown Atlanta Homeless Shelter", "Atlanta Union Mission", "Fuqua Hall Transitional Housing", "Gateway Center Atlanta"]
     var shelterAddress: [String] = ["625 Spencer Street NW, Atlanta, GA 30318", "469 Marietta Street, Atlanta, GA 30313", "Atlanta, GA, 30314", "165 Ivan Allen Blvd NW, Atlanta, GA 30313", "165 Alexander Street NW, Atlanta, GA 30301", "144 Mills Street, Atlanta, GA 30313", "275 Pryor St., SW, Atlanta, GA 30303"]
+    var resultsReleasedAt : [Int] = [3, 2, 1, 3, 3, 4, 5]
     var selected : [Bool] = [false, false, false, false, false, false, false]
     var applied : [Bool] = [false, false, false, false, false, false, false]
     
--- SquarePassionProject/ViewResultsViewController.swift
+++ SquarePassionProject/ViewResultsViewController.swift
@@ -14,9 +14,20 @@ class ViewResultsViewController: UIViewController {
     @IBOutlet weak var result2: UILabel!
     @IBOutlet weak var tbdLabel: UILabel!
     @IBOutlet weak var waitlistLabel: UILabel!
-    
+    var resultsReleasedAt : [Int] = [3, 2, 1, 3, 3, 4, 5]
+    var appliedShelters : [Bool]?
+    var shelterNames: [String] = ["Odyssey Villas Intact Families", "The Salvation Army Metro Atlanta Red Shield Service", "Serenity House of Atlanta", "The Shepherd's Inn Downtown Atlanta Homeless Shelter", "Atlanta Union Mission", "Fuqua Hall Transitional Housing", "Gateway Center Atlanta"]
+    var shelterAddress: [String] = ["625 Spencer Street NW, Atlanta, GA 30318", "469 Marietta Street, Atlanta, GA 30313", "Atlanta, GA, 30314", "165 Ivan Allen Blvd NW, Atlanta, GA 30313", "165 Alexander Street NW, Atlanta, GA 30301", "144 Mills Street, Atlanta, GA 30313", "275 Pryor St., SW, Atlanta, GA 30303"]
+    var hour = 12
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Get applied array:
+        let defaults = UserDefaults.standard
+        appliedShelters = defaults.array(forKey: "SavedApplied") as? [Bool] ?? [Bool]()
+        let date = Date()
+        let calendar = Calendar.current
+        
+        hour = calendar.component(.hour, from: date)
         
         // Do any additional setup after loading the view.
     }
@@ -25,5 +36,13 @@ class ViewResultsViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    func displayNotYetResults() {
+        var arrayOfTBDShelters = [String]()
+        for i in 0 ..< resultsReleasedAt.count {
+            if(resultsReleasedAt[i] > hour && (appliedShelters?[i])!) {
+                //Display this in the not yet released:
+                arrayOfTBDShelters.append(shelterNames[i])
+            }
+        }
+    }
 }
